7e066a0780e8e1903c907ff810dc53413954c7d5
Jakub Kocourek <jakub.kocourek@fregis.cz>
2013-10-21 11:34:02 +0200

    Added annotation filter

---
+++ GettextExtractor/Filters/AnnotationFilter.php
@@ -0,0 +1,38 @@
+<?php
+
+class GettextExtractor_Filters_AnnotationFilter implements GettextExtractor_Filters_IFilter
+{
+	public function extract($file)
+	{
+		$pInfo = pathinfo($file);
+		$data = array();
+		foreach (file($file) as $line => $contents) {
+			$line++;
+			preg_match_all('#'.
+				'\*.*@[\w]+\((.+)\)'. // annotations
+				'#', $contents, $matches);
+			if (empty($matches)) continue;
+			if (empty($matches[0])) continue;
+			foreach ($matches[1] as $match) {
+				if($match == "") continue;
+               	$msgs = preg_split("/,/", $match);
+				foreach($msgs as $msg) {
+					$msg = trim($msg);
+					if(($start = strpos($msg, "= \"")) !== FALSE)  {
+						$msg = substr($msg, $start+2);
+					}
+					elseif(($start = strpos($msg, "=\"")) !== FALSE)  {
+						$msg = substr($msg, $start+1);
+					}
+
+					if((substr($msg, 0, 1) == "\"") || (substr($msg, 0, 1) == "'")) {
+						$msg = substr($msg, 1, -1);
+					}
+
+					$data[$msg][] = $pInfo['basename'] . ':' . $line;
+				}
+			}
+		}
+		return $data;
+	}
+}
